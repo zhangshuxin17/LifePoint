ca076e8a59a145e4e9cd8a7aea6ada1bb0a0cab8
zhangshuxin17 <31028017+zhangshuxin17@users.noreply.github.com>
2020-10-06 19:22:01 +0800

    remove useless code

--- ThinkerGuass/水光/软件/MesoPro/Device/tuiye_motor.c
+++ ThinkerGuass/水光/软件/MesoPro/Device/tuiye_motor.c
@@ -7,9 +7,7 @@ void goRun(int type)
 {
 		// 10K  36000
 	TIM_OC_InitTypeDef sConfigOC = {0};
-	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-		
+
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = type == 0 ? 3600 : 1800;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
@@ -32,8 +30,7 @@ void goRunFast(void)
 
 		// 10K  36000
 	TIM_OC_InitTypeDef sConfigOC = {0};
-	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
 		
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
   sConfigOC.Pulse = 36000;
