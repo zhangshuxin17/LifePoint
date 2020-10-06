1b3347d2d652a408cd98033316b9de0e91464927
zhangshuxin17 <31028017+zhangshuxin17@users.noreply.github.com>
2020-10-06 23:22:05 +0800

    20HZ

--- ThinkerGuass/水光/软件/MesoPro/Src/main.c
+++ ThinkerGuass/水光/软件/MesoPro/Src/main.c
@@ -336,9 +336,9 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 720;
+  htim2.Init.Prescaler = 360;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 5000;
+  htim2.Init.Period = 10000 - 1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -361,7 +361,7 @@ static void MX_TIM2_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
+  sConfigOC.Pulse = 5000;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
@@ -372,6 +372,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 2 */
   HAL_TIM_MspPostInit(&htim2);
+
 
 }
 
@@ -641,11 +642,7 @@ static void MX_TIM8_Init(void)
   /* USER CODE END TIM8_Init 2 */
   HAL_TIM_MspPostInit(&htim8);
 	
-		HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_1);
-		HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_2);
-		HAL_TIM_PWM_Start(&htim8,TIM_CHANNEL_3);
-	
-	while(1);
+
 
 }
 
